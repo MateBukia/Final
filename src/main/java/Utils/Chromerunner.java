9a34c9583ee8bd2412a8cf29cc80d00a3dc0eb26
MateBukia <bukiamate@gmail.com>
2023-03-13 21:57:42 +0400

    Final

--- src/main/java/Utils/Chromerunner.java
+++ src/main/java/Utils/Chromerunner.java
@@ -2,18 +2,9 @@ package Utils;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-<<<<<<< HEAD
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-=======
-import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.By;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-import static com.codeborne.selenide.Selenide.$;
->>>>>>> 3dfb8c0b062052ae9a7e780686c41167dda168bb
 import static com.codeborne.selenide.Selenide.open;
 
 public class Chromerunner {
@@ -29,7 +20,6 @@ public class Chromerunner {
         Selenide.closeWindow();
         Selenide.closeWebDriver();
     }
-<<<<<<< HEAD
 
 
 
@@ -38,6 +28,3 @@ public class Chromerunner {
       public     String    mypassword = "Paroli345%";
 }
 
-=======
-}
->>>>>>> 3dfb8c0b062052ae9a7e780686c41167dda168bb
